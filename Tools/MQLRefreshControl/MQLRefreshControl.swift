3efddeca831926277d6b253dcbb08174958d5e79
maqianli <maqianli@qding.me>
2018-03-22 22:21:02 +0800

    自定义刷新控件

---
+++ Tools/MQLRefreshControl/MQLRefreshControl.swift
@@ -0,0 +1,22 @@
+//
+//  MQLRefreshControl.swift
+//  WeiBo
+//
+//  Created by QDHL on 2018/3/22.
+//  Copyright © 2018年 QDHL. All rights reserved.
+//
+
+import UIKit
+
+class MQLRefreshControl: UIControl {
+    
+    var refreshing: Bool = false
+
+    func beginRefreshing() {
+        
+    }
+    
+    func endRefreshing() {
+        
+    }
+}
