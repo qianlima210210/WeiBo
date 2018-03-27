315ac353b6c396677061bd5757e932ae84ad67ad
maqianli <maqianli@qding.me>
2018-03-27 12:07:37 +0800

    WB_1.0.0_撰写类型

---
+++ View/Main/ComposeTypeView/ComposeTypeButton.swift
@@ -0,0 +1,21 @@
+//
+//  ComposeTypeButton.swift
+//  WeiBo
+//
+//  Created by QDHL on 2018/3/27.
+//  Copyright © 2018年 QDHL. All rights reserved.
+//
+
+import UIKit
+
+class ComposeTypeButton: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
